b96a7d92030bee75b5a3799652fa02af31b282df
Ibraheem Muhammadawwal <Ibraheemmuhdawwal@gmail.com>
2023-03-17 01:25:53 +0100

    more functionalitites

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const session = require('express-session');
 const http = require('http');
 const { Server } = require('socket.io');
 const dotenv = require('dotenv');
+const path = require('path');
 
 // Set up the server
 dotenv.config();
@@ -12,8 +13,7 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 // Serve static files from the public directory
-app.use(express.static('public'));
-
+app.use(express.static(path.join(__dirname, 'public')));
 // Use session middleware
 const sessionMiddleware = session({
   secret: process.env.SECRET_KEY,
@@ -31,18 +31,65 @@ io.engine.use(sessionMiddleware);
 // Define the fast foods and order history and orderHistory
 
 const fastFoods = {
-  2: 'Jollof',
-  3: 'Indomie',
-  4: 'Poundo',
-  5: 'Semo',
+  100: {
+    name: 'Burger',
+    price: 100,
+  },
+  101: {
+    name: 'Pizza',
+    price: 200,
+  },
+  102: {
+    name: 'Fries',
+    price: 50,
+  },
+  103: {
+    name: 'Sandwich',
+    price: 150,
+  },
+  104: {
+    name: 'Pasta',
+    price: 250,
+  },
+  105: {
+    name: 'Noodles',
+    price: 300,
+  },
+  106: {
+    name: 'Biryani',
+    price: 400,
+  },
+  107: {
+    name: 'Dosa',
+    price: 500,
+  },
+  109: {
+    name: 'Idli',
+    price: 600,
+  },
+  110: {
+    name: 'Puri',
+    price: 700,
+  },
+  111: {
+    name: 'Chapati',
+    price: 800,
+  },
+  112: {
+    name: 'Paratha',
+    price: 900,
+  },
 };
 
 const orderHistory = [];
 
+// Define the menu string return name and price as name@#price
 const getMenuString = () => {
-  return Object.keys(fastFoods)
-    .map((key) => `${key}. ${fastFoods[key]}`)
-    .join('\n');
+  let menuString = '';
+  for (const key in fastFoods) {
+    menuString += `${key}. ${fastFoods[key].name} @ ₦${fastFoods[key].price}\n`;
+  }
+  return menuString;
 };
 
 // Define the socket.io event listeners
@@ -55,6 +102,15 @@ io.on('connection', (socket) => {
     socket.emit('bot-message', message);
   };
 
+  // Define the state
+  const initialState = {
+    selectedMenuItem: null,
+    waitingForQuantity: false,
+    checkoutConfirmation: false,
+  };
+
+  let state = { ...initialState };
+
   // Ask for the user's name
   sendBotMessage("Hello! What's your name?");
 
@@ -65,43 +121,94 @@ io.on('connection', (socket) => {
   let userName = '';
 
   const handleUserInput = (message) => {
-    switch (message) {
-      case '1':
+    if (state.waitingForQuantity) {
+      const quantity = parseInt(message);
+      if (isNaN(quantity)) {
+        sendBotMessage('Invalid quantity. Please enter a number.');
+        return;
+      }
+      const selectedItem = state.selectedMenuItem;
+      const orderItem = {
+        name: selectedItem.name,
+        quantity: quantity,
+        price: selectedItem.price * quantity,
+      };
+      socket.request.session.currentOrder.push(orderItem);
+      sendBotMessage(
+        `${quantity}. ${selectedItem.name}@₦${selectedItem.price} =  ₦${
+          selectedItem.price * quantity
+        } has been added to your cart. Do you want to add more items to your cart?\n 1. See menu.\n99. to checkout. \n00. Main menu`
+      );
+      // Reset the state
+      state = { ...initialState };
+    } else if (state.checkoutConfirmation) {
+      if (message === '1') {
+        // Add the current order to the order history
+        orderHistory.push(socket.request.session.currentOrder);
+        // Send the order summary
         sendBotMessage(
-          `Here is a list of items you can order:\n ${getMenuString()}\n Please select one by typing its number.`
+          'Your order has been placed. Thank you for shopping with us.\n00. Return to Main Menu'
         );
-        break;
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-        handleItemSelection(parseInt(message));
-        break;
-      case '99':
-        handleCheckout();
-        break;
-      case '98':
-        handleOrderHistory();
-        break;
-      case '97':
-        handleCurrentOrder();
-        break;
-      case '0':
-        handleOrderCancellation();
-        break;
-      default:
-        sendBotMessage('Invalid selection. Please try again.');
-        break;
+      } else {
+        sendBotMessage(
+          'Your order has been cancelled. Thank you for shopping with us.\n00. Return to Main Menu'
+        );
+      }
+      // Reset the state
+      state = { ...initialState };
+      // Reset the current order
+      socket.request.session.currentOrder = [];
+    } else {
+      switch (message) {
+        case '1':
+          sendBotMessage(
+            `Here is a list of items you can order:\n ${getMenuString()}\n Please select one\n00 Return to main menu.`
+          );
+          break;
+        case '100':
+        case '101':
+        case '102':
+        case '103':
+        case '104':
+        case '105':
+        case '106':
+        case '107':
+        case '108':
+        case '109':
+        case '110':
+        case '111':
+        case '112':
+          handleItemSelection(parseInt(message));
+          break;
+        case '99':
+          handleCheckout();
+          break;
+        case '98':
+          handleOrderHistory();
+          break;
+        case '97':
+          handleCurrentOrder();
+          break;
+        case '0':
+          handleOrderCancellation();
+          break;
+        case '00':
+          handleMainMenu();
+          break;
+        default:
+          sendBotMessage('Invalid selection. Please try again.');
+          break;
+      }
     }
   };
 
   const handleItemSelection = (selectedIndex) => {
     if (fastFoods.hasOwnProperty(selectedIndex)) {
       const selectedItem = fastFoods[selectedIndex];
-      socket.request.session.currentOrder.push(selectedItem);
-      sendBotMessage(
-        `${selectedItem} has been added to your order. Do you want to add more items to your order?\n 1. See menu.\n If not, type 99 to checkout.`
-      );
+      sendBotMessage(`How many ${selectedItem.name} do you want to order?`);
+      // Set the state to keep track of the selected item and wait for the quantity
+      state.selectedMenuItem = selectedItem;
+      state.waitingForQuantity = true;
     } else {
       sendBotMessage('Invalid selection.');
     }
@@ -111,9 +218,21 @@ io.on('connection', (socket) => {
     if (socket.request.session.currentOrder.length === 0) {
       sendBotMessage('No order to place. Place an order\n1. See menu');
     } else {
-      orderHistory.push(socket.request.session.currentOrder);
-      sendBotMessage('Order placed');
-      socket.request.session.currentOrder = [];
+      const totalPrice = socket.request.session.currentOrder.reduce(
+        (acc, item) => acc + item.price,
+        0
+      );
+      const orderItems = socket.request.session.currentOrder
+        .map((item) => `${item.name} x ${item.quantity} = ₦${item.price}\n`)
+        .join(', ');
+      sendBotMessage(
+        `Order placed \n` +
+          `Your order is ${orderItems}\nTotal: ${totalPrice}\n\n` +
+          `1. Confirm order\n0. Cancel Order`
+      );
+
+      // Set the state to keep track of the checkout confirmation
+      state.checkoutConfirmation = true;
     }
   };
 
@@ -122,9 +241,16 @@ io.on('connection', (socket) => {
       sendBotMessage('No previous orders');
     } else {
       const orderHistoryString = orderHistory
-        .map((order, index) => `Order ${index + 1}: ${order.join(', ')}`)
+        .map((order, index) => {
+          const totalPrice = order.reduce((acc, item) => acc + item.price, 0);
+          return `Order ${index + 1}: ${order.map(
+            (item) => `${item.name} x ${item.quantity} = ₦${item.price}.\n`
+          )}\nTotal: ${totalPrice}\n`;
+        })
         .join('\n');
-      sendBotMessage(`Here is your order history:\n ${orderHistoryString}`);
+      sendBotMessage(
+        `Here is your order history:\n${orderHistoryString}\n\n00 Return to main menu`
+      );
     }
   };
 
@@ -132,14 +258,31 @@ io.on('connection', (socket) => {
     if (socket.request.session.currentOrder.length === 0) {
       sendBotMessage('No current order. Place an order\n1. See menu');
     } else {
-      const currentOrderString = socket.request.session.currentOrder.join(', ');
-      sendBotMessage(`Here is your current order: ${currentOrderString}`);
+      const totalPrice = socket.request.session.currentOrder.reduce(
+        (acc, item) => acc + item.price,
+        0
+      );
+      const orderItems = socket.request.session.currentOrder.map(
+        (item) => `${item.name} x ${item.quantity} = ₦${item.price}.\n`
+      );
+
+      sendBotMessage(`Here is your current order:
+      
+      ${orderItems}\nTotal: ${totalPrice}\n\nDo you want to place this order?\n99. Checkout\n0. Cancel Order\n00. Return to main menu`);
     }
   };
 
   const handleOrderCancellation = () => {
     socket.request.session.currentOrder = [];
-    sendBotMessage('Order cancelled. Place a new order\n1. See menu');
+    sendBotMessage(
+      'Order cancelled. Place a new order\n1. See menu\n00 Return to main menu'
+    );
+  };
+
+  const handleMainMenu = () => {
+    sendBotMessage(
+      `Welcome to the ChatBot, ${userName}!\n1. Place an Order\n99. Checkout\n98. Order History\n97. Current Order\n0. Cancel Order`
+    );
   };
 
   // Listen for incoming user messages
@@ -149,7 +292,7 @@ io.on('connection', (socket) => {
     if (!userName) {
       userName = message;
       sendBotMessage(
-        `Welcome to the ChatBot, ${userName}!\n1. Place an Order\n99. Checkout\n98. Current Order\n97. Order History \n0. Cancel Order`
+        `Welcome to the ChatBot, ${userName}!\n1. Place an Order\n99. Checkout\n98. Order History\n97. Current Order \n0. Cancel Order`
       );
     } else {
       handleUserInput(message);
